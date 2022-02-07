6faeee30b53a00489b859d68aa5ac02161c75080
netovieira01_ <76048368+manoelvieira0@users.noreply.github.com>
2022-02-07 08:44:57 -0300

    Reutilizando ainda mais

--- modelo.py
+++ modelo.py
@@ -21,17 +21,15 @@ class Programa:
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self._nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self._likes = 0
+
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self._nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self._likes = 0
+
 
 vingadores = Filme("vingadores - guerra infinita", 2018, 160)
 vingadores.dar_like()
