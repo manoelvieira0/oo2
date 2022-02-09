e6ce6fe06bc0b99b3dcd8764dde0c98b58216b96
netovieira01_ <76048368+manoelvieira0@users.noreply.github.com>
2022-02-09 07:18:12 -0300

    Criando a playlist

--- modelo.py
+++ modelo.py
@@ -38,12 +38,32 @@ class Serie(Programa):
     def __str__(self):
         return f'{self._nome} - {self.ano} - {self.temporadas} Temporadas - {self._likes} Likes'
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self.programas = programas
+
+    def tamanho(self):
+        return len(self.programas)
+
 vingadores = Filme("vingadores - guerra infinita", 2018, 160)
-
 supernatural = Serie("supernatural", 2007, 15)
+duna = Filme('duna', 2021, 130)
+flash = Serie('flash', 2014, 6)
 
-filmes_e_series = [vingadores, supernatural]
+vingadores.dar_like()
+vingadores.dar_like()
+supernatural.dar_like()
+supernatural.dar_like()
+duna.dar_like()
+duna.dar_like()
+duna.dar_like()
+flash.dar_like()
 
-for programa in filmes_e_series:
+filmes_e_series = [vingadores, supernatural, duna, flash]
+
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
+
+for programa in playlist_fim_de_semana.programas:
     print(programa)
 
